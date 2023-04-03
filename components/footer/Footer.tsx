c0f78c1bb5f9d06ed616810c9385dfd65cc222a1
lucasnull <lucasnull@live.com>
2023-04-03 08:09:43 -0300

    feat: desktop ok

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -69,7 +69,7 @@ function Footer({ sections = [] }: Props) {
             <Newsletter />
           </FooterContainer>
 
-          <FooterContainer class="flex flex-row text-white gap-5">
+          <FooterContainer class="flex flex-row text-white gap-5 md:place-self-end">
             <Icon
               id={"Instagram"}
               width={25}
@@ -102,130 +102,105 @@ function Footer({ sections = [] }: Props) {
             />
           </FooterContainer>
 
-          <FooterContainer class="text-white">
-            <p>
-              Espaço para curta descrição do negócio, não permitir que sejam
-              escritas muitas coisas aqui pra não deixar o rodapé muito grande.
-            </p>
-            <br />
-            <p>Av. Cândido Hartmann - Mercês - Curitiba - Paraná - Brasil</p>
-            <br />
-            <p class="font-bold">email@dominio.com.br</p>
-          </FooterContainer>
+          <div class="md:flex md:flex-row md:divide-x-2 md:divide-default">
+            <FooterContainer class="text-white md:w-[25%]">
+              <p>
+                Espaço para curta descrição do negócio, não permitir que sejam
+                escritas muitas coisas aqui pra não deixar o rodapé muito
+                grande.
+              </p>
+              <br />
+              <p>Av. Cândido Hartmann - Mercês - Curitiba - Paraná - Brasil</p>
+              <br />
+              <p class="font-bold">email@dominio.com.br</p>
+            </FooterContainer>
 
-          <hr class="mx-5" />
+            <hr class="mx-5 md:hidden" />
 
-          <FooterContainer>
-            {/* Desktop view */}
-            <ul class="hidden sm:flex flex-row gap-20">
-              {sections.map((section) => (
-                <li>
-                  <div>
-                    <Text variant="heading-3" tone="default-inverse">
-                      {section.label}
-                    </Text>
+            <FooterContainer>
+              {/* Desktop view */}
+              <ul class="hidden sm:flex flex-row gap-60 ml-5">
+                {sections.map((section) => (
+                  <li>
+                    <div>
+                      <Text variant="heading-3" tone="default-inverse">
+                        {section.label}
+                      </Text>
 
-                    <ul
-                      class={`flex ${
-                        isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                      } gap-2 pt-2`}
-                    >
-                      {section.children.map((item) => (
-                        <li>
-                          <SectionItem item={item} />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              ))}
-            </ul>
-
-            {/* Mobile view */}
-            <ul className="flex flex-col h-full lg:hidden text-white gap-3">
-              {sections.map((section, index) => {
-                return (
-                  <li key={index} class="flex flex-col">
-                    <div
-                      class="font-bold uppercase flex flex-row gap-5"
-                      onClick={() => setActiveSectionIndex(index)}
-                    >
-                      {section.label}
-                      <div>
-                        <Icon
-                          id="ChevronDown"
-                          width={20}
-                          height={20}
-                          strokeWidth={3}
-                        />
-                      </div>
+                      <ul
+                        class={`flex ${
+                          isIcon(section.children[0]) ? "flex-row" : "flex-col"
+                        } gap-2 pt-2`}
+                      >
+                        {section.children.map((item) => (
+                          <li>
+                            <SectionItem item={item} />
+                          </li>
+                        ))}
+                      </ul>
                     </div>
-
-                    {index === activeSectionIndex && (
-                      <div class="p-2 max-h-screen flex flex-1 flex-col">
-                        {section.children.map((item) => {
-                          return <SectionItem item={item} />;
-                        })}
-                      </div>
-                    )}
                   </li>
-                );
-              })}
-            </ul>
-          </FooterContainer>
-          <hr class="mx-5" />
+                ))}
+              </ul>
+
+              {/* Mobile view */}
+              <ul className="flex flex-col h-full lg:hidden text-white gap-3">
+                {sections.map((section, index) => {
+                  return (
+                    <li key={index} class="flex flex-col">
+                      <div
+                        class="font-bold uppercase flex flex-row gap-5"
+                        onClick={() => setActiveSectionIndex(index)}
+                      >
+                        {section.label}
+                        <div>
+                          <Icon
+                            id="ChevronDown"
+                            width={20}
+                            height={20}
+                            strokeWidth={3}
+                          />
+                        </div>
+                      </div>
+
+                      {index === activeSectionIndex && (
+                        <div class="p-2 max-h-screen flex flex-1 flex-col">
+                          {section.children.map((item) => {
+                            return <SectionItem item={item} />;
+                          })}
+                        </div>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            </FooterContainer>
+          </div>
+          <hr class="mx-5 md:hidden" />
         </Container>
       </div>
 
-      <div>
+      <div class="md:place-self-end md:m-3">
         <Container class="w-full">
-          <FooterContainer class="flex justify-between w-full">
-            <Text
-              class="flex items-center gap-1"
-              variant="body"
-              tone="default-inverse"
-            >
-              Powered by{" "}
-              <a
-                href="https://www.deco.cx"
-                aria-label="powered by https://www.deco.cx"
-              >
-                <Icon id="Deco" height={20} width={60} strokeWidth={0.01} />
-              </a>
-            </Text>
-
-            <ul class="flex items-center justify-center gap-2">
+          <FooterContainer class="flex w-full">
+            <ul class="flex justify-end gap-2">
               <li>
-                <a
-                  href="https://www.instagram.com/deco.cx"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Instagram logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Instagram"
-                    strokeWidth={1}
-                  />
-                </a>
+                <Icon
+                  class="text-default-inverse"
+                  width={32}
+                  height={32}
+                  id="Visa"
+                  strokeWidth={1}
+                />
               </li>
               <li>
-                <a
-                  href="http://www.deco.cx/discord"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Discord logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Discord"
-                    strokeWidth={5}
-                  />
-                </a>
+                <Icon
+                  class="text-default-inverse"
+                  width={32}
+                  height={32}
+                  id="Mastercard"
+                  strokeWidth={1}
+                />
               </li>
             </ul>
           </FooterContainer>
