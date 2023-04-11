e54635632c3f50a803617f598838ce007438ae5e
lucasnull <lucasnull@live.com>
2023-04-11 12:23:20 -0300

    feat: create products sections

---
+++ components/product/ProductDescription.tsx
@@ -0,0 +1,7 @@
+export default function ProductDescription() {
+  return (
+    <>
+      <h1>ProductDescription</h1>
+    </>
+  );
+}
---
+++ components/product/ProductListingTitle.tsx
@@ -0,0 +1,7 @@
+export default function ProductListingTitle() {
+  return (
+    <>
+      <h1>ProductListingTitle</h1>
+    </>
+  );
+}
