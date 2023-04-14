22ac6d5cb8b68ef71c25a42e0b1bef86a6793c94
SoulRomullo <soulromullo@gmail.com>
2023-04-13 21:55:32 -0300

    imp: altering in banner size response

--- components/product/ProductListingTitle.tsx
+++ components/product/ProductListingTitle.tsx
@@ -1,11 +1,11 @@
 export default function ProductListingTitle() {
     return (
-        <div className="flex relative mb-[6vw] h-[24vw] bg-[#F5F5FB]">
+        <div className="md:h-[15rem] flex relative mb-[6vw] h-[15rem] bg-[#F5F5FB]">
             <div className="flex absolute items-center bottom-0 justify-start left-[5%] m-auto right-[5%] top-0">
-                <div className="md:w-[50%] md:text-left text-center w-[100%] h-fit p-8 font-montserrat text-[#B92C38]">
-                    <span className="text-[1.4vw] leading-[1] mb-[0.5rem] uppercase">Busca por:</span>
-                    <h1 className="font-black text-[3vw] leading-[1.2] mb-[0]">Categoria</h1>
-                    <p className="font-semibold text-[2.5vw] leading-[1.2] mb-[1rem] margin-0">9 produtos
+                <div className="md:w-[50%] md:text-left p-[20px] w-[100%] font-montserrat h-fit text-center text-[#B92C38]">
+                    <span className="md:text-[12px] text-[12px] tracking-wider font-semibold leading-[1] mb-[4px] uppercase">Busca por:</span>
+                    <h1 className="md:text-[28px] tracking-wider font-black text-[28px] leading-[1.2] mb-[0]">Categoria</h1>
+                    <p className="md:text-[22px] font-semibold tracking-wider text-[22px] leading-[1.2] mb-[10px] margin-0">9 produtos
                         encontrados.</p>
                 </div>
             </div>
